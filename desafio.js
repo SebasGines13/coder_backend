7c5e646996e018a92acbec77393575befcd3ff5e
SebaGines <sebastian.gines@gmail.com>
2023-07-25 19:42:17 -0300

    Adding final files

--- desafio.js
+++ desafio.js
@@ -15,6 +15,25 @@ class ProductManager {
     }
     return "Not found";
   }
+
+  addProduct(product) {
+    if (this.products.find((p) => p.code == product.code)) {
+      return "Producto ya existente";
+    }
+
+    if (
+      product.title != "" ||
+      product.description != "" ||
+      product.price > 0 ||
+      product.thumbnail != "" ||
+      product.code != "" ||
+      product.stock > 0
+    ) {
+      this.products.push(product);
+    } else {
+      return "Debe ingresar valores en todos los campos!";
+    }
+  }
 }
 
 class Product {
@@ -25,5 +44,41 @@ class Product {
     this.thumbnail = thumbnail;
     this.code = code;
     this.stock = stock;
+    this.id = Product.nextId();
+  }
+
+  // Método de clase
+  static nextId() {
+    if (this.idIncrement) {
+      // Atributo de la clase.
+      this.idIncrement++;
+    } else {
+      this.idIncrement = 1;
+    }
+    return this.idIncrement;
   }
 }
+
+const product1 = new Product(
+  "Cafe la Morenita",
+  "Cafe la Morenita en su empaque de 2kg.",
+  1998,
+  "wwww.C1234.com",
+  "C1234",
+  100
+);
+const product2 = new Product(
+  "Yerba Playadito",
+  "Yerba Playadito en su empaque de 1kg",
+  800,
+  "wwww.Y1234.com",
+  "Y1234",
+  250
+);
+
+const productManager = new ProductManager();
+
+productManager.addProduct(product1);
+productManager.addProduct(product2);
+
+console.log(productManager.getProducts());
