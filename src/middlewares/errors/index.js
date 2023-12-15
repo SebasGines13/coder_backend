adf704e555cbeabbaada585884f547ffffbf40cc
SebaGines <sebastian.gines@gmail.com>
2023-12-14 21:28:58 -0300

    Fix enum

--- src/middlewares/errors/index.js
+++ src/middlewares/errors/index.js
@@ -1,10 +1,10 @@
-import { EErrors } from "../../services/errors/enum.js";
+import { eerrors } from "../../services/errors/enum.js";
 import logger from "../../utils/logger.js";
 
 export default (error, req, res, next) => {
   logger.error(error.message);
   switch (error.code) {
-    case EErrors.MISSING_REQUIRED_FIELDS:
+    case eerrors.MISSING_REQUIRED_FIELDS:
       res.status(400).send({ status: "error", error: error.name });
       break;
     default:
