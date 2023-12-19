01a4664d8562e38791ff612817b734791b7dadbf
SebaGines <sebastian.gines@gmail.com>
2023-12-19 19:42:19 -0300

    [Fix] const errors

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,7 +1,7 @@
 import productModel from "../models/products.models.js";
 import { generateProduct } from "../utils/utils.js";
 import CustomError from "../services/errors/customError.js";
-import { EErrors } from "../services/errors/enum.js";
+import EErrors from "../services/errors/enum.js";
 import { generateProductErrorInfo } from "../services/errors/Info.js";
 
 const getProducts = async (req, res) => {
--- src/services/errors/Enum.js
+++ src/services/errors/Enum.js
@@ -1,8 +1,6 @@
-export const EErrors = {
+export default {
   MISSING_REQUIRED_FIELDS: {
     ROUTING_ERROR: 1,
     INVALID_TYPES_ERROR: 2,
   },
 };
-
-export default EErrors;
